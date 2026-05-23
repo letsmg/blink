f461836add658b6b28c5867894f1300ab182110a
Luiz Eduardo <luiztsilva@protonmail.com>
2026-05-23 17:15:27 -0300

    fix: adiciona icon.png como favicon e corrige referência no welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="index, follow">
-        <link rel="icon" type="image/x-icon" href="/favicon.ico">
+
+        {{-- Favicon --}}
+        <link rel="icon" type="image/png" href="{{ asset('icon.png') }}">
 
         {{-- SEO Meta Tags Base --}}
         <title>@yield('title', 'Blink - Sistema Inteligente de Gestão de Clínicas e Consultórios')</title>
